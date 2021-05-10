d6830d7b37f202ece0e67c12bb5438c2f437dbad
joshmorenx <37154328+joshmorenx@users.noreply.github.com>
2021-05-10 15:24:14 -0500

    Fixed deleting main.php from the layouts folder

--- mooove.sh
+++ mooove.sh
@@ -4,3 +4,7 @@ mv /var/www/html/humhub-1.8.2/protected/humhub/views/layouts/main.php main-origi
 echo "renaming the modified main site to the humhub index"
 mv /var/www/html/humhub-1.8.2/protected/humhub/views/layouts/main1.php main.php
 echo "done!"
+
+echo "moving the main.php to its original location"
+mv main.php /var/www/html/humhub-1.8.2/protected/humhub/views/layouts/
+echo "done"
